b8a68e14cee1c35bacfe88b744f6714fb974ea2d
Rory Ellis <rellis@c4r8s7.wethinkcode.co.za>
2020-02-18 15:13:21 +0200

    Moved files, created ft_swap, ft_len, ft_cmp

---
+++ functions/ft_putstr.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void    ft_putstr(const char *str)
+{
+	int i;
+
+	if (!str)
+	{
+		ft_putchar('\0');
+	}
+	else
+	{
+		i = 0;
+		while (str[i] != '\0')
+		{
+			ft_putchar(str[i]);
+			i++;
+		}
+	}
+}
+
+int		main(void)
+{
+	const char *str;
+
+	str = "Hello world";
+	ft_putstr(str);
+	return (0);
+}
---
+++ functions/ft_swap.c
@@ -0,0 +1,8 @@
+void	ft_swap(int *a, int *b)
+{
+	int c;
+
+	int c = *b;
+	*b = *a;
+	*a = c;
+}
